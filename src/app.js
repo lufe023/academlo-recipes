dccf90e30cbb33b23d44fde0915263e046c9fb4c
lufe023 <lufe023@gmail.com>
2022-11-03 18:00:22 -0400

    final project

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 //? Dependencies
 const express = require('express');
+const cors = require('cors')
 const db = require('./utils/database')
 
 //? Files
@@ -7,12 +8,18 @@ const {port} = require('./config');
 //* Routes
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
+const categoryRouter = require('./categories/categories.router')
+const recipeRouter = require('./recipes/recipes.router')
+const ingredientRouter = require('./ingredients/ingredients.router')
+
 const initModels = require('./models/initModels')
 
 //? Initial Configs
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 db.authenticate()
     .then(() => {
@@ -42,6 +49,9 @@ app.get('/',(req, res) => {
 
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/categories', categoryRouter)
+app.use('/api/v1/recipes', recipeRouter)
+app.use('/api/v1/ingredients', ingredientRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
